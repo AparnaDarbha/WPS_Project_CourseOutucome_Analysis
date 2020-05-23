ce02b5d6fa786247c7e7738d27ce3c6f07a5e277
AparnaDarbha <60744713+AparnaDarbha@users.noreply.github.com>
2020-05-23 18:35:19 +0530

    Update callexcel.js

--- route/callexcel.js
+++ route/callexcel.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
 router.get('/', function(req, res){ 
-    res.render('excel');//thats it?
+    res.render('excel');
 });
 module.exports = router;
