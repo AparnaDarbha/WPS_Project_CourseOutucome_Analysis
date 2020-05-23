9a7221277fa49165df121e406b38f06b111ba9ba
AparnaDarbha <60744713+AparnaDarbha@users.noreply.github.com>
2020-05-23 18:34:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,6 @@ app.get('/', function(request, response) {
 app.use('/int_ss',int_ss);
 app.use('/courses',co_);
 app.use('/callexcel',ce_);
-//we will try? yes
 app.use('/ext_ss',ext_ss);
 app.use('/read_frm_xl',read_frm_xl);
 //app.use('/edit',edit);
@@ -50,15 +49,12 @@ app.use('/QandAssign',quiz_ss);
 
 app.use('/demo',demo);
 app.use('/demo1',demo1);
-app.use('/demo2',demo2);//is this enough? yes
-//app.use('/storeCO',sc);//now we can execute ok
+app.use('/demo2',demo2);
 app.use('/cs',cs);
 app.get('/set_date', function(request, response) {
-        response.render('set_date');//why isnt datetime having calendar or something yes even i thought tat
-        //maybe datetime doesnt exust
+        response.render('set_date');
         response.end();
     });
-//});//why do we have an error here? im checing that 
 app.get('/h', function(request, response) {
     var username=request.session.username;
     connection.query('SELECT course FROM courses WHERE Name=?', [username], function(error, results, fields) {
@@ -74,7 +70,7 @@ app.get('/h', function(request, response) {
         response.end();
     });
 });
-app.post('/auth', function(request, response) {//shall e directly give /auth? 
+app.post('/auth', function(request, response) {
     var username = request.body.username;
     var password = request.body.password;
     if (username && password) {
@@ -83,19 +79,14 @@ if(error){
     console.log(error);
     return;
 }
-//when can we extrct sysdate anywhere we can
             if (results.length > 0) {
                 
                 request.session.loggedin = true;
                 request.session.username = username;
                 if(username=="admin")
-                response.redirect('/set_date');//we will create a page to set date and time? ok ejs? yes
+                response.redirect('/set_date');
                 else
-                response.redirect('/h');//if it is admin we will redirect ok this way maybe we can
-                //i thougth we will read val in a separate page and then redirect it and do remaining cal in int_ss
-                //maybe it will work we will try and see ok
-                //did u write ml assign? i dont know anything in thag i will see at night i found 2nd ans half in internet
-                //i got full i thikn 2nd ans i will see and let u know ok
+                response.redirect('/h');
             } else {
                 response.send('Incorrect Username and/or Password!');
             }           
