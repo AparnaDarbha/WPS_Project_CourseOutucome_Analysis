b5e8127abca46965653ef7d5ea24cfb51a5ff377
AparnaDarbha <60744713+AparnaDarbha@users.noreply.github.com>
2020-05-23 18:42:46 +0530

    Update disp_questions.js

--- route/disp_questions.js
+++ route/disp_questions.js
@@ -16,12 +16,7 @@ var express = require('express');
     {
         for(let j=0;j<=4;j++)
         {
-            console.log(req.body['checkbox'+i]);//we will use this rugth ok
-            //i think it will push all values or should we specify value? we will check before pushing
-            //is this crt i think this is enough maybe what abt each individual checkbox in this grp?i dont know we will see
-            //we will execute and see? ok 
-            //i am thinking its bettrt to do cn we wont include this part then? this looks time taking yes 
-            //we will do later then  first cn ok
+            console.log(req.body['checkbox'+i]);
         }
         console.log(val);
     }
